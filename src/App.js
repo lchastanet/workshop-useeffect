8e63ab3407afe47c466a561028586b83670ffce8
Loris <45939615+lchastanet@users.noreply.github.com>
2022-04-11 17:12:23 +0200

    first step
    Co-authored-by: Antho-37 <Antho-37@users.noreply.github.com>
    Co-authored-by: FREDERIQUE DEMAS <frederique.demas73@gmail.com>
    Co-authored-by: ClemDSC <ClemDSC@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./styles/App.css"
+import Clock from "./components/Clock"
+import React from "react"
 
 function App() {
-  return <div className="App"></div>
+  const [showClock, setShowClock] = React.useState(true)
+
+  function handleShowClock() {
+    setShowClock(!showClock)
+  }
+
+  return (
+    <div className="App">
+      {showClock ? <Clock /> : null}
+
+      <button onClick={handleShowClock}>button</button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Clock.jsx
@@ -0,0 +1,17 @@
+import react, { useState } from "react"
+
+function Clock () {
+
+const [hour, setHour] = useState("");
+    
+return (
+    
+    <div>
+        <p>Clock</p>
+    </div>    
+    
+)
+
+}
+
+export default Clock
