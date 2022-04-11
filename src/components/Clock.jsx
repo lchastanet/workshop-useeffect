32e33f8af098e220c980c633fbbe8bf34a273844
Loris <45939615+lchastanet@users.noreply.github.com>
2022-04-11 17:34:48 +0200

    last step

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function Clock () {
 
 const [date, setDate] = useState(new Date());
+
+useEffect(() => {
+    console.log("setting up interval")
+    const interval = setInterval(() => {
+        setDate(new Date())
+        console.log("date updated")
+    }, 1000);
+    return () => {
+        console.log("clearing interval");
+        clearInterval(interval)
+    }
+}, [] )
 
 return (
     
